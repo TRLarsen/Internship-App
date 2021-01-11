60eeb3944a969535bdfa84572311c9d989b48917
TRLarsen <67074921+TRLarsen@users.noreply.github.com>
2021-01-11 14:03:58 -0700

    Formatting for server post requests.

--- tyler.js
+++ tyler.js
@@ -1,4 +1,8 @@
-var obj_in = {"data": {"in_or_out":"In", "date_and_time":Date(), "full_name":"null", "email":"null", "message":"null"}}
+var obj_in = {
+    "packet":"ClockIn",
+    "lastName": "null",
+    "firstName":"null", 
+}
 var intern = "out"
 
 //Button look/content Change
@@ -7,29 +11,27 @@ function clickFunction() {
         document.querySelector("button").style.backgroundColor = "red";
         document.querySelector("button").innerText = "Out"
         intern = "in"
-        obj_in.data.in_or_out = "Out"
         console.log("Clocked In:")}
     else if (intern == "in") {
         document.querySelector("button").style.backgroundColor = "green";
         document.querySelector("button").innerText = "In"
         intern = "out"
-        obj_in.data.in_or_out = "In"
         console.log("Clocked Out:")
     }
     console.log(Date())
 }
 
 //Submit to Google Sheet through apispreadsheets
-
 function SubForm (){
-  obj_in.data.full_name = myForm.full_name.value;
-  obj_in.data.email = myForm.email.value;
-  obj_in.data.message = myForm.message.value;
-  obj_in.data.date_and_time = Date()
+  obj_in.firstName = myForm.firstName.value;
+  obj_in.lastName = myForm.lastName.value;
     $.ajax({
-        url:'https://api.apispreadsheets.com/data/5119/',
+        url:'https://jkcoxson.com',
         type:'post',
         data: JSON.stringify(obj_in), //https://www.apispreadsheets.com/guides/update-data-subset-rows
+        callback: (data) => {
+          console.log(data)
+        },
      success: function(){
         console.log("Form Data Submitted :)")
      },
